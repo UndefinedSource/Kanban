6f3843d5826658dff1c15747f9bab3dbc3d2c62b
UndefinedSource <UndefinedSource>
2020-09-07 19:33:26 -0400

    Updates

--- precache-manifest.1296d019f2d1704c6cae910ba0ebf113.js
+++ precache-manifest.1296d019f2d1704c6cae910ba0ebf113.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "38bed1140b0c4ec3b0ca244055d3cb45",
+    "revision": "9cef33f4bafb6cfd082050446104d0c5",
     "url": "/kanban/index.html"
   },
   {
-    "revision": "fd473a41ca52fb04442f",
-    "url": "/kanban/static/css/main.fac1706d.chunk.css"
+    "revision": "502c535857d518e39231",
+    "url": "/kanban/static/css/main.c4bbd930.chunk.css"
   },
   {
     "revision": "391fe813f5e785186fd0",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kanban/static/js/2.db17a86d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fd473a41ca52fb04442f",
+    "revision": "502c535857d518e39231",
     "url": "/kanban/static/js/main.d9a85a61.chunk.js"
   },
   {
